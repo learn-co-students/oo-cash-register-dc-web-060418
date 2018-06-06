41b47d5addd29da83ece9bf2abed86d4089d558a
github username <github email address>
2018-06-06 15:50:41 -0400

    OO Cash Register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
 
+class CashRegister
+
+    attr_accessor :total, :discount
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+        @transactions = []
+    end
+
+    def add_item(title, price, quantity = 1)        
+        quantity.times do
+            @items << title
+            @transactions << price
+        end
+        self.total += (price * quantity)
+    end
+        
+
+    def apply_discount()
+        if @discount == 0
+            return "There is no discount to apply."
+        else
+            @total -= @total * @discount/100
+            return "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @items.pop
+        @total -= @transactions.pop
+    end
+end
